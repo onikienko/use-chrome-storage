61d05747d576f7ae701cbe08dfb937956bf25988
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2021-06-29 19:06:41 +0300

    fix: handle case when "defaultValue" for storage.get is "undefined"

--- src/storage.js
+++ src/storage.js
@@ -1,13 +1,13 @@
 export const storage = {
     get: (key, defaultValue, storageArea) => {
+        const keyObj = defaultValue === undefined ? key : {[key]: defaultValue};
         return new Promise((resolve, reject) => {
-            chrome.storage[storageArea].get({[key]: defaultValue}, items => {
+            chrome.storage[storageArea].get(keyObj, items => {
                 const error = chrome.runtime.lastError;
                 if (error) return reject(error);
                 resolve(items[key]);
             });
         });
-
     },
     set: (key, value, storageArea) => {
         return new Promise((resolve, reject) => {
